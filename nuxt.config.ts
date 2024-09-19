ac6d317a2086c1e5d822aee24117334d7a6233bb
Marvin <josemarvin.ra@reliasourcing.com>
2024-09-19 14:42:55 +0800

    removed type=module in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    devServer: {
+        port: 8000
+      },    
     compatibilityDate: "2024-04-03",
     runtimeConfig: {
         public: {
